10b6fe44a30c2a210d0262bad88cb9896c8581d1
Загир Валитов <zagir-valitov@outlook.com>
2024-10-24 20:09:09 +0500

    Добавьте файлы проекта.

---
+++ OtusDictionary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Basic_11_L035;
+
+internal class OtusDictionary()
+{
+    public static int Capacity = 32;
+    public static int Size; 
+   
+    private struct Entry
+    {
+        public int Key;
+        public string? Value;
+    }
+    private Entry[] _words = new Entry[Capacity];    
+    public string? this[int k]
+    {
+        get
+        { 
+            return Get(k);            
+        }
+        set
+        {
+            Add(k, value);
+        }
+    }
+    private int GetHashKey(int key)
+    {
+        return key % Capacity;        
+    }
+    public void Add(int key, string? value) 
+    {
+        if (value is not null)
+        {
+            int hashKey = GetHashKey(key);
+            if (_words[hashKey].Value is null)
+            {
+                _words[hashKey].Key = key;
+                _words[hashKey].Value = value;
+                Size++;
+            }
+            else
+            {
+                var buffer = new Entry[Capacity];
+                Array.Copy(_words, 0, buffer, 0, Capacity - 1);
+
+                Capacity *= 2;
+                _words = new Entry[Capacity];
+                                
+                foreach (var word in buffer)
+                {
+                    _words[GetHashKey(word.Key)] = word;                    
+                }
+                Add(key, value);
+            }
+        }
+    }
+    public string? Get(int key)
+    {
+        if (key >= 0 && GetHashKey(key) <= Capacity)
+        {
+            return _words[GetHashKey(key)].Value;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using HomeWork_Basic_11_L035;
+
+Console.WriteLine(" ---- Home work 11 ----\n");
+
+OtusDictionary dictionary = new OtusDictionary();
+
+dictionary.Add(1, "Республика Адыгея ");
+dictionary.Add(2, "Республика Башкортостан ");
+dictionary.Add(3, "Республика Бурятия");
+dictionary.Add(4, "Республика Алтай");
+dictionary.Add(5, "Республика Дагестан");
+dictionary[6] = "Республика Ингушетия";
+dictionary[7] = "Кабардино-Балкарская Республика";
+dictionary[8] = "Республика Калмыкия";
+dictionary[9] = "Республика Карачаево-Черкесия";
+dictionary[32] = "Республика Карелия";
+dictionary[66] = "Свердловская область";
+dictionary[69] = "Тверская область";
+
+try
+{
+    Console.WriteLine(dictionary.Get(1));
+    Console.WriteLine(dictionary.Get(2));
+    Console.WriteLine(dictionary.Get(3));
+    Console.WriteLine(dictionary.Get(4));
+    Console.WriteLine(dictionary.Get(5));
+    Console.WriteLine(dictionary[6]);
+    Console.WriteLine(dictionary[7]);
+    Console.WriteLine(dictionary[8]);
+    Console.WriteLine(dictionary[9]);
+    Console.WriteLine(dictionary[32]);
+    Console.WriteLine(dictionary[66]);
+    Console.WriteLine(dictionary[69]);
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
